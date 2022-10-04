9e33021309f3bdaa2bf905b73953e6342057c3a6
modengann <99417989+modengann@users.noreply.github.com>
2022-10-04 08:36:15 -0400

    init

---
+++ src/App.java
@@ -0,0 +1,22 @@
+/**
+ * Write code so:
+ * 1. On start, a Rectangle is created that is centered in the middle of the 
+ * Window with a width of 100 and a height of 70. To clarify, 
+ * “centered” means that the center of the Rectangle will initially 
+ * be in the exact center of the Window (x: 400, y: 300)
+ * 
+ * 2. When the mouse is clicked, the Rectangle should move up 5 pixels.
+ * 
+ * For extra achievement (eligible for A+):
+ * Keep the previous parts intact, but also demonstrate higher levels of 
+ * understanding in a way of your choosing.
+ */
+
+
+import javadraw.*;
+
+public class App extends Window {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
