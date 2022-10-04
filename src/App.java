9537187f3dd741f11d89aab8111961f379ce2beb
casey <cpmoden@gmail.com>
2022-10-04 08:59:23 -0400

    Remove hello world

--- src/App.java
+++ src/App.java
@@ -17,6 +17,6 @@ import javadraw.*;
 
 public class App extends Window {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
     }
 }
